3f64468d7ee0437059e170cb65c90ef30db21bb6
katsu224 <aguilarfabianluis@gmail.com>
2026-02-16 08:40:52 -0500

    SSR2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node'; // Add this line
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server'
 });
